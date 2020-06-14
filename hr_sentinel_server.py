42e40de1f3352274cf8ca4c4574058110f2346df
bradhoward1 <bradley.howard@duke.edu>
2020-06-14 14:19:44 -0400

    Function to add attending physician to list

--- hr_sentinel_server.py
+++ hr_sentinel_server.py
@@ -1,1 +1,14 @@
 # hr_sentinel_server.py
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+attending_db = list()
+
+
+def add_new_attending(username_id, email, phone):
+    new_attending = {"attending_username": username_id,
+                     "attending_email": email,
+                     "attending_phone": phone}
+    attending_db.append(new_attending)
+    return True
--- test_hr_sentinel_server.py
+++ test_hr_sentinel_server.py
@@ -1,1 +1,11 @@
 # test_hr_sentinel_server.py
+
+
+def test_add_new_attending():
+    from hr_sentinel_server import add_new_attending
+    input_name = "Howard.B"
+    input_email = "bradhtest@test.com"
+    input_phone = "919-867-5309"
+    answer = add_new_attending(input_name, input_email, input_phone)
+    expected = True
+    assert answer == expected
