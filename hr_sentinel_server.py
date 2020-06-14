c7e239f296a8c901bdbcfb1f7e01849ee811300c
bradhoward1 <bradley.howard@duke.edu>
2020-06-14 14:34:31 -0400

    Function to validate keys and types of new attending physician input dictionary

--- hr_sentinel_server.py
+++ hr_sentinel_server.py
@@ -12,3 +12,15 @@ def add_new_attending(username_id, email, phone):
                      "attending_phone": phone}
     attending_db.append(new_attending)
     return True
+
+
+def validate_new_attending(new_dict):
+    expected_keys = ("attending_username",
+                     "attending_email", "attending_phone")
+    expected_types = (str, str, str)
+    for key, types in zip(expected_keys, expected_types):
+        if key not in new_dict.keys():
+            return "{} key not found".format(key)
+        if type(new_dict[key]) != types:
+            return "{} key is the wrong value type".format(key)
+    return True
--- test_hr_sentinel_server.py
+++ test_hr_sentinel_server.py
@@ -1,5 +1,7 @@
 # test_hr_sentinel_server.py
 
+import pytest
+
 
 def test_add_new_attending():
     from hr_sentinel_server import add_new_attending
@@ -8,4 +10,22 @@ def test_add_new_attending():
     input_phone = "919-867-5309"
     answer = add_new_attending(input_name, input_email, input_phone)
     expected = True
+    assert answer == expected
+
+
+@pytest.mark.parametrize("result, expected",
+                         [({"attending_username": "Howard.B",
+                            "attending_email": "bradhtest@test.com",
+                            "attending_phone": "919-867-5309"}, True),
+                          ({"attending_username": "Howard.B",
+                            "attending_email": "bradhtest@test.com",
+                            "attending_phone": 919-867-5309}, 'attending_phone'
+                          ' key is the wrong value type'),
+                          ({"attending_user": "Howard.B",
+                            "attending_email": "bradhtest@test.com",
+                            "attending_phone": "919-867-5309"},
+                          'attending_username key not found')])
+def test_validate_new_attending(result, expected):
+    from hr_sentinel_server import validate_new_attending
+    answer = validate_new_attending(result)
     assert answer == expected
