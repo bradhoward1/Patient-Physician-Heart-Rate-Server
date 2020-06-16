f2e51ce885cb159779a93cce2fe5cc2b718f257a
Rob Baldoni <robert.baldoni@duke.edu>
2020-06-16 16:23:46 -0400

    Development and testing of /api/heart_rate/interval_average

--- test_hr_sentinel_server.py
+++ test_hr_sentinel_server.py
@@ -122,6 +122,27 @@ def test_add_patient_hr(result1, result2, expected):
     assert answer == expected
 
 
+def test_avg_hr_calc():
+    from hr_sentinel_server import avg_hr_calc
+    patient_hr_db = [{"patient_id": 1,
+                      "heart_rate": [80, 90, 160],
+                      "timestamp": ["2018-03-09 11:00:36",
+                                    "2018-03-09 11:10:36",
+                                    "2018-03-09 11:20:36"]},
+                     {"patient_id": 2,
+                      "heart_rate": [70, 80],
+                      "timestamp": ["2020-07-10 1:30:50",
+                                    "2020-07-10 1:50:50"]},
+                     {"patient_id": 3,
+                      "heart_rate": [50, 60, 70],
+                      "timestamp": ["2018-03-09 11:00:36",
+                                    "2018-03-09 11:20:36",
+                                    "2018-03-09 11:50:36"]}]
+    answer = avg_hr_calc(patient_hr_db, 3, "2018-03-09 11:20:36")
+    expected = 65, True
+    assert answer == expected
+
+
 @pytest.mark.parametrize("result1, result2, expected",
                          [(1, 152, True),
                           (1, 151, False),
